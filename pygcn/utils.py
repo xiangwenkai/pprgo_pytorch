e1b36061ec0155abe516b512c8598ba937097b2a
Xiang, Wenkai (FSG) <wenkai.xiang@astrazeneca.com>
2022-09-26 13:49:26 +0800

    GCN code

---
+++ pygcn/utils.py
@@ -0,0 +1,148 @@
+import numpy as np
+import scipy.sparse as sp
+import torch
+import sklearn
+from pprgo.sparsegraph import load_from_npz
+from pprgo.utils import SparseRowIndexer, split_random
+from torch_geometric.datasets import Reddit
+
+
+def encode_onehot(labels):
+    classes = set(labels)
+    classes_dict = {c: np.identity(len(classes))[i, :] for i, c in
+                    enumerate(classes)}
+    labels_onehot = np.array(list(map(classes_dict.get, labels)),
+                             dtype=np.int32)
+    return labels_onehot
+
+
+def load_data(path="../data/cora/", dataset="cora"):
+    """Load citation network dataset (cora only for now)"""
+    print('Loading {} dataset...'.format(dataset))
+
+    idx_features_labels = np.genfromtxt("{}{}.content".format(path, dataset),
+                                        dtype=np.dtype(str))
+    features = sp.csr_matrix(idx_features_labels[:, 1:-1], dtype=np.float32)
+    labels = encode_onehot(idx_features_labels[:, -1])
+
+    # build graph
+    idx = np.array(idx_features_labels[:, 0], dtype=np.int32)
+    idx_map = {j: i for i, j in enumerate(idx)}
+    edges_unordered = np.genfromtxt("{}{}.cites".format(path, dataset),
+                                    dtype=np.int32)
+    edges = np.array(list(map(idx_map.get, edges_unordered.flatten())),
+                     dtype=np.int32).reshape(edges_unordered.shape)
+    adj = sp.coo_matrix((np.ones(edges.shape[0]), (edges[:, 0], edges[:, 1])),
+                        shape=(labels.shape[0], labels.shape[0]),
+                        dtype=np.float32)
+
+    # build symmetric adjacency matrix
+    adj = adj + adj.T.multiply(adj.T > adj) - adj.multiply(adj.T > adj)
+
+    features = normalize(features)
+    adj = normalize(adj + sp.eye(adj.shape[0]))
+
+    idx_train = range(140)
+    idx_val = range(200, 500)
+    idx_test = range(500, 1500)
+
+    features = torch.FloatTensor(np.array(features.todense()))
+    labels = torch.LongTensor(np.where(labels)[1])
+    adj = sparse_mx_to_torch_sparse_tensor(adj)
+
+    idx_train = torch.LongTensor(idx_train)
+    idx_val = torch.LongTensor(idx_val)
+    idx_test = torch.LongTensor(idx_test)
+
+    return adj, features, labels, idx_train, idx_val, idx_test
+
+
+def load_npz_data(path, seed, ntrain_div_classes):
+    # path = '../data/reddit.npz'
+    g = load_from_npz(path)
+
+    # number of nodes and attributes
+    n, d = g.attr_matrix.shape
+
+    features = sp.csr_matrix(g.attr_matrix, dtype=np.float32)
+
+    # split the data into train/val/test
+    num_classes = g.labels.max() + 1
+    n_train = num_classes * ntrain_div_classes
+    n_val = n_train
+    idx_train, idx_val, idx_test = split_random(seed, n, n_train, n_val)
+    adj = g.adj_matrix.tocoo()
+    # build symmetric adjacency matrix
+    adj = adj + adj.T.multiply(adj.T > adj) - adj.multiply(adj.T > adj)
+
+    features = normalize(features)
+    adj = normalize(adj + sp.eye(adj.shape[0]))
+
+    features = torch.FloatTensor(np.array(features.todense()))
+    labels = torch.LongTensor(g.labels)
+    adj = sparse_mx_to_torch_sparse_tensor(adj)
+
+    idx_train = torch.LongTensor(idx_train)
+    idx_val = torch.LongTensor(idx_val)
+    idx_test = torch.LongTensor(idx_test)
+    return adj, features, labels, idx_train, idx_val, idx_test
+
+
+def load_geometric_data():
+    # path = '../data/reddit.npz'
+    dataset = Reddit('../data/Reddit')
+    data = dataset[0]
+
+    features = sp.csr_matrix(data['x'], dtype=np.float32)
+
+    # split the data into train/val/test
+    # idx_train = torch.nonzero(data['train_mask']==True).squeeze()
+    # idx_val = torch.nonzero(data['val_mask']==True).squeeze()
+    # idx_test = torch.nonzero(data['test_mask']==True).squeeze()
+    idx_train, idx_val, idx_test = split_random(seed=0, n=len(data.x), n_train=800, n_val=8000)
+
+    edges = data.edge_index.T
+    adj = sp.coo_matrix((np.ones(edges.shape[0]), (edges[:, 0], edges[:, 1])),
+                        shape=(data.y.shape[0], data.y.shape[0]),
+                        dtype=np.float32)
+    # build symmetric adjacency matrix
+    adj = adj + adj.T.multiply(adj.T > adj) - adj.multiply(adj.T > adj)
+
+    features = normalize(features)
+    adj = normalize(adj + sp.eye(adj.shape[0]))
+
+    features = torch.FloatTensor(np.array(features.todense()))
+    labels = torch.LongTensor(data.y)
+    adj = sparse_mx_to_torch_sparse_tensor(adj)
+
+    idx_train = torch.LongTensor(idx_train)
+    idx_val = torch.LongTensor(idx_val)
+    idx_test = torch.LongTensor(idx_test)
+    return adj, features, labels, idx_train, idx_val, idx_test
+
+
+def normalize(mx):
+    """Row-normalize sparse matrix"""
+    rowsum = np.array(mx.sum(1))
+    r_inv = np.power(rowsum, -1).flatten()
+    r_inv[np.isinf(r_inv)] = 0.
+    r_mat_inv = sp.diags(r_inv)
+    mx = r_mat_inv.dot(mx)
+    return mx
+
+
+def accuracy(output, labels):
+    preds = output.max(1)[1].type_as(labels)
+    correct = preds.eq(labels).double()
+    correct = correct.sum()
+    return correct / len(labels)
+
+
+def sparse_mx_to_torch_sparse_tensor(sparse_mx):
+    """Convert a scipy sparse matrix to a torch sparse tensor."""
+    sparse_mx = sparse_mx.tocoo().astype(np.float32)
+    indices = torch.from_numpy(
+        np.vstack((sparse_mx.row, sparse_mx.col)).astype(np.int64))
+    values = torch.from_numpy(sparse_mx.data)
+    shape = torch.Size(sparse_mx.shape)
+    return torch.sparse.FloatTensor(indices, values, shape)
